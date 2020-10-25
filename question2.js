f35b15c7349c38756aee3f77de663c65d680c28c
Baaa0130 <69729099+Baaa0130@users.noreply.github.com>
2020-10-25 23:20:29 +0100

    Add files via upload

---
+++ question2.js
@@ -0,0 +1,42 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+const container = document.querySelector(".container");
+const handler = document.querySelector(".handler");
+​
+async function call() {
+   
+        const response = await fetch(url);
+        const results = await response.json();
+        const facts = results.results;
+        
+        container.innerHTML = "";
+        for(i=0; i<facts.length; i++) 
+        {
+            if (i === 8){
+                break;
+            }
+            // container.innerHTML += facts[i].name + " " 
+            // + facts[i].rating + " " 
+            // + facts[i].tags.length;
+            
+            // container.innerHTML += `${facts[i].name} 
+            // ${facts[i].rating} 
+            // ${facts[i].tags.length}`;
+​
+            const html = `
+            <div>
+                <p> ${facts[i].name} </p>
+                <p> ${facts[i].rating} </p>
+                <p> ${facts[i].tags.length} </p>
+            </div>
+            `
+            container.innerHTML += html;    
+​
+        }
+} 
+try{
+call();
+handler.innerHTML = "success";
+}
+catch(error) {
+    handler.innerHTML = "error found";
+}
